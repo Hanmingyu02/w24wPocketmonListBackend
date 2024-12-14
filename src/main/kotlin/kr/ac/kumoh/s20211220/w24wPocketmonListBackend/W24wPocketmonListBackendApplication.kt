a78cd7cc6fb410101fd1853ecc0888ddc37beb33
Mingyu <hmg0835@naver.com>
2024-12-15 02:20:00 +0900

    🙈 MongoDB 연결 성공

---
+++ src/main/kotlin/kr/ac/kumoh/s20211220/w24wPocketmonListBackend/W24wPocketmonListBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20211220.w24wPocketmonListBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24wPocketmonListBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24wPocketmonListBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20211220/w24wPocketmonListBackend/W24wPocketmonListBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20211220.w24wPocketmonListBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24wPocketmonListBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
