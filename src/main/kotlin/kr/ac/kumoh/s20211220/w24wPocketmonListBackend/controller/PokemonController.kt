37e9eace4cf10d4871269cdd410526ec31774ae6
Mingyu <hmg0835@naver.com>
2024-12-15 02:49:36 +0900

    🚀 Backend 완성

--- src/main/kotlin/kr/ac/kumoh/s20211220/w24wPocketmonListBackend/controller/PokemonController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20211220/w24wPocketmonListBackend/controller/PokemonController.kt
@@ -2,11 +2,31 @@ package kr.ac.kumoh.s20211220.w24wPocketmonListBackend.controller
 
 import kr.ac.kumoh.s20211220.w24wPocketmonListBackend.model.Pokemon
 import kr.ac.kumoh.s20211220.w24wPocketmonListBackend.service.PokemonService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/pokemons")
 class PokemonController(private val service: PokemonService) {
-    @GetMapping("/api/pokemons")
-    fun getAllPokemon(): List<Pokemon> = service.getAllPokemons()
+    @PostMapping
+    fun addPokemon(@RequestBody pokemon: Pokemon): Pokemon = service.addPokemon(pokemon)
+
+    @GetMapping
+    fun getAllPokemons(): List<Pokemon> = service.getAllPokemons()
+
+    @GetMapping("/{id}")
+    fun getPokemonById(@PathVariable id: String): Pokemon? = service.getPokemonById(id)
+
+    @GetMapping("/name/{name}")
+    fun getPokemonByName(@PathVariable name: String): List<Pokemon> = service.getPokemonByName(name)
+
+    @PutMapping("/{id}")
+    fun updatePokemon(@PathVariable id: String, @RequestBody pokemonDetails: Pokemon): Pokemon? =  service.updateSong(id, pokemonDetails)
+
+    @DeleteMapping("/{id}")
+    fun deletePokemon(@PathVariable id: String): Map<String, String> {
+        return if (service.deletePokemon(id))
+            mapOf("status" to "deleted")
+        else
+            mapOf("status" to "not found")
+    }
 }
--- src/main/kotlin/kr/ac/kumoh/s20211220/w24wPocketmonListBackend/service/PokemonService.kt
+++ src/main/kotlin/kr/ac/kumoh/s20211220/w24wPocketmonListBackend/service/PokemonService.kt
@@ -6,5 +6,34 @@ import org.springframework.stereotype.Service
 
 @Service
 class PokemonService(private val repository: PokemonRepository) {
+    fun addPokemon(pokemon: Pokemon): Pokemon = repository.save(pokemon)
+
     fun getAllPokemons(): List<Pokemon> = repository.findAll()
+    fun getPokemonById(id: String): Pokemon? = repository.findById(id).orElse(null)
+    fun getPokemonByName(name: String): List<Pokemon> = repository.findByName(name)
+
+    fun updateSong(id: String, pokemon: Pokemon): Pokemon? {
+        val pokemonTarget = repository.findById(id)
+
+        return if (pokemonTarget.isPresent) {
+            val oldPokemon = pokemonTarget.get()
+            val updatedPokemon = oldPokemon.copy(
+                number = pokemon.number,
+                name = pokemon.name,
+                explanation = pokemon.explanation,
+            )
+            repository.save(updatedPokemon)
+        } else {
+            null
+        }
+    }
+
+    fun deletePokemon(id: String): Boolean {
+        return if (repository.existsById(id)) {
+            repository.deleteById(id)
+            true
+        } else {
+            false
+        }
+    }
 }
